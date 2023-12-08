d54bb4502020231395d5d1b86a3fd3f21fc11888
WilliamReis-304 <153219919+WilliamReis-304@users.noreply.github.com>
2023-12-08 15:31:45 -0300

    Add files via upload

--- rpg/Classes.py
+++ rpg/Classes.py
@@ -12,6 +12,11 @@ class Guerreiro(Classes):
         super().__init__(2,2,2,2,2,2)
         self.forca = self.forca + 3
         self.defesa = self.defesa + 2
+        self. agilidade = self.agilidade - 1
+        self.magia = self.magia - 1
+    
+    def maestria():
+        return True
 
 class Ladino(Classes):
     def __init__(self):
@@ -20,6 +25,9 @@ class Ladino(Classes):
         self.destreza = self.destreza + 3
         self.magia = self.magia + 1
 
+    def furtividade():
+        return True
+
 class Mago(Classes):
     def __init__(self):
         super().__init__(2,2,2,2,2,2)
@@ -27,8 +35,10 @@ class Mago(Classes):
         self.inteligencia = self.inteligencia + 1
         self.magia = self.magia + 3
 
+    def magia_inicial():
+        return True
+
 class Paladino(Classes):
-    # def __init__(self, forca : int, defesa : int, magia : int, destreza : int):
     def __init__(self):
         super().__init__(2,2,2,2,2,2)
         self.forca = self.forca + 2
@@ -36,10 +46,5 @@ class Paladino(Classes):
         self.magia = self.magia + 2
         self.inteligencia = self.inteligencia +1
 
-
-
-# player = Classes(2,2,2,2,2,2)
-
-# print(player.__dict__)
-# print(player.forca)
-
+    def reza():
+        return True
